31d8293eb37ee9eb7708c4f17bbb9a04b26cff07
ando <anmutu@hotmail.com>
2017-08-09 14:41:21 +0800

    版本冲突解决。点击“修改密码”后，再点击其他菜单会报错的bug修复。

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 __author__ = 'Ando'
 __date__ = '8/2/2017 11:56 PM'
 
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -11,3 +11,10 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+# 404页面
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"), 404
+
