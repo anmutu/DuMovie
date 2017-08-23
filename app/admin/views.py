b1d16664d640ff49e368d3d41e272c0c77068ffc
ando <anmutu@hotmail.com>
2017-08-23 15:53:34 +0800

    tag标签CURD

--- app/admin/views.py
+++ app/admin/views.py
@@ -31,7 +31,7 @@ def index():
 
 # 登录
 @admin.route("/login/", methods=["GET", "POST"])
-#@admin_login_req   login没有装饰器，不然会导致
+# @admin_login_req   login没有装饰器，不然会导致重定向，访问不了
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -79,9 +79,29 @@ def tag_add():
     return render_template("admin/tag_add.html", form=form)
 
 
+# 编辑标签
+@admin.route("/tag/edit/<int:id>/", methods=["GET", "POST"])
+@admin_login_req
+def tag_edit(id=None):
+    form = TagForm()
+    tag = Tag.query.get_or_404(id)
+    if form.validate_on_submit():
+        data = form.data
+        tag_count = Tag.query.filter_by(name=data["name"]).count()
+        if tag.name != data["name"] and tag_count == 1:
+            flash("名称已经存在！", "err")
+            return redirect(url_for('admin.tag_edit', id=id))
+        tag.name = data["name"]
+        db.session.add(tag)
+        db.session.commit()
+        flash("修改标签成功！", "ok")
+        redirect(url_for('admin.tag_edit', id=id))
+    return render_template("admin/tag_edit.html", form=form, tag=tag)
+
+
 # 标签列表
 @admin.route("/tag/list/<int:page>/", methods=["GET"])
-def tag_list():
+def tag_list(page=None):
     if page is None:
         page = 1
     page_data = Tag.query.order_by(
@@ -90,7 +110,17 @@ def tag_list():
     return render_template("admin/tag_list.html", page_data=page_data)
 
 
-# 添加电影
+# 标签删除
+@admin.route("/tag/del/<int:id>/", methods=["GET"])
+@admin_login_req
+def tag_del(id=None):
+    tag = Tag.query.filter_by(id=id).first_or_404()
+    db.session.delete(tag)
+    db.session.commit()
+    flash("删除标签成功！", "ok")
+    return redirect(url_for('admin.tag_list', page=1))  # 添加电影
+
+
 @admin.route("/movie_add/")
 def movie_add():
     return render_template("admin/movie_add.html")
@@ -156,9 +186,22 @@ def role_list():
     return render_template("admin/role_list.html")
 
 
+# 角色修改
+@admin.route("/role/edit/")
+def role_edit():
+    return render_template("admin/role_edit.html")
+
+
+
+# 角色删除
+@admin.route("/role/del/")
+def role_del():
+    return render_template("admin/role_del.html")
+
+
 # 管理员日志
 @admin.route("/adminloginlog/list/")
-def adminlgoinlog_list():
+def adminloginlog_list():
     return render_template("admin/adminloginlog_list.html")
 
 
