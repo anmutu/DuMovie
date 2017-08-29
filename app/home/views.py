f2afb67f63d37f3bcd159c1d1631935c493e1ad5
ando <anmutu@hotmail.com>
2017-08-29 21:25:49 +0800

    前台用户注册。

--- app/home/views.py
+++ app/home/views.py
@@ -1,3 +1,15 @@
+from . import home
+from flask import render_template, redirect, url_for, request, session, flash, Response
+# 实体
+from app.models import User, UserLog
+# 表单
+from app.home.forms import RegistForm, LoginForm
+# 数据库
+from app import db
+# 密码判断使用
+from werkzeug.security import generate_password_hash
+import uuid
+
 # _*_ coding:utf-8 _*_
 __author__ = 'Ando'
 __date__ = '8/15/2017 10:52 PM'
@@ -6,8 +18,24 @@ __date__ = '8/15/2017 10:52 PM'
 __author__ = 'Ando'
 __date__ = '8/2/2017 11:58 PM'
 
-from . import home
-from flask import render_template, redirect, url_for
+
+# 会员注册
+@home.route("/register/", methods=["GET", "POST"])
+def register():
+    form = RegistForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User(
+            name=data["name"],
+            email=data["email"],
+            phone=data["phone"],
+            pwd=generate_password_hash(data["pwd"]),
+            uuid=uuid.uuid4().hex
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("注册成功！", "ok")
+    return render_template("home/register.html", form=form)
 
 
 @home.route("/")
@@ -17,21 +45,32 @@ def index():
 
 
 # 登录
-@home.route("/login/")
-def logout():
-    return render_template("home/login.html")
-
-
-# 退出
-@home.route("/logout/")
+@home.route("/login/", methods=["GET", "POST"])
 def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User.query.filter_by(name=data["name"]).first()
+        if not user.check_pwd(data["pwd"]):
+            flash("密码错误！", "err")
+            return redirect(url_for("home.login"))
+        session["user"] = user.name
+        session["user_id"] = user.id
+        userlog = UserLog(
+            user_id=user.id,
+            ip=request.remote_addr
+        )
+        db.session.add(userlog)
+        db.session.commit()
+        return redirect(url_for("home.user"))
+    return render_template("home/login.html", form=form)
+
+
+@home.route("/logout/")
+def logout():
+    session.pop("user", None)
+    session.pop("user_id", None)
     return redirect(url_for("home.login"))
-
-
-# 注册
-@home.route("/register/")
-def register():
-    return render_template("home/register.html")
 
 
 # 用户
